5ae9917d8962f69928c80b1cfe2864230b47f700
Alex Banai <46088872+Codename14@users.noreply.github.com>
2024-06-12 15:34:53 +0200

    build: header, add react-icons, style: search item move to single file

---
+++ components/Header.tsx
@@ -0,0 +1,18 @@
+import Link from 'next/link';
+import React from 'react';
+import { FaGithub } from 'react-icons/fa';
+
+export default function Header() {
+    return (
+        <>
+            <header>
+                <nav className='flex justify-between limit-width p-2'>
+                    <h2 className='text-2xl'>Hello</h2>
+                    <Link href='/'>
+                        <FaGithub size={40} />
+                    </Link>
+                </nav>
+            </header>
+        </>
+    );
+}
